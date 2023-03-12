553cbd1d509f88950099f1b45a96a54af5806b33
thisismemukul <w41k3r.12@gmail.com>
2023-03-12 15:01:59 +0530

    Step 1 - Setting up a Java Spring Project using htttp://start.spring.io

--- spring-in-5-steps/src/main/java/com/springcourse/spring/basics/springin5steps/SpringIn5StepsApplication.java
+++ spring-in-5-steps/src/main/java/com/springcourse/spring/basics/springin5steps/SpringIn5StepsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringIn5StepsApplication {
-
+//Step 1 - Setting up a Java Spring Project using htttp://start.spring.io
 	public static void main(String[] args) {
 		SpringApplication.run(SpringIn5StepsApplication.class, args);
 	}
