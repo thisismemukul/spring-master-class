99df988e942eba565a3c7e0fda78313f1a52480f
thisismemukul <w41k3r.12@gmail.com>
2023-03-14 21:33:31 +0530

    Step 6 - Dynamic auto wiring and Troubleshooting - @Primary

--- spring-in-5-steps/src/main/java/com/springcourse/spring/basics/springin5steps/QuickSortAlgorithm.java
+++ spring-in-5-steps/src/main/java/com/springcourse/spring/basics/springin5steps/QuickSortAlgorithm.java
@@ -1,5 +1,8 @@
 package com.springcourse.spring.basics.springin5steps;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class QuickSortAlgorithm implements SortAlgorithm{
     public int[] sort(int[] numbers){
     //logic for bubble sort
