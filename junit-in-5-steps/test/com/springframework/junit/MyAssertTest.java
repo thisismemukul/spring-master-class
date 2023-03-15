31967a26f693296eb9b6b46b5f1defc283335105
thisismemukul <w41k3r.12@gmail.com>
2023-03-16 03:38:16 +0530

    Step 4 : Other assert methods

--- junit-in-5-steps/test/com/springframework/junit/MyAssertTest.java
+++ junit-in-5-steps/test/com/springframework/junit/MyAssertTest.java
@@ -2,13 +2,20 @@ package com.springframework.junit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 class MyAssertTest {
+	List<String> todos = Arrays.asList("AWS", "Azure", "DevOPS");
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		boolean test = todos.contains("AWS");
+//		assertEquals(true, test);
+		assertTrue("Something went wrong",test);
+		assertEquals(3,todos.size(),"Error Message");
 	}
 
 }
