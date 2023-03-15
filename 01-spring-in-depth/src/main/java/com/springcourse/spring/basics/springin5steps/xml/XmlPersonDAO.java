d9a225a682e0362f057b928d3bc3d036b6b8f9dd
thisismemukul <w41k3r.12@gmail.com>
2023-03-16 01:22:55 +0530

    Step 21 - Defining Spring Application Context using XML - Part 1

---
+++ 01-spring-in-depth/src/main/java/com/springcourse/spring/basics/springin5steps/xml/XmlPersonDAO.java
@@ -0,0 +1,13 @@
+package com.springcourse.spring.basics.springin5steps.xml;
+
+public class XmlPersonDAO {
+    XmlJdbcConnection xmlJdbcConnection;
+
+    public XmlJdbcConnection getXmlJdbcConnection() {
+        return xmlJdbcConnection;
+    }
+
+    public void setXmlJdbcConnection(XmlJdbcConnection xmlJdbcConnection) {
+        this.xmlJdbcConnection = xmlJdbcConnection;
+    }
+}
