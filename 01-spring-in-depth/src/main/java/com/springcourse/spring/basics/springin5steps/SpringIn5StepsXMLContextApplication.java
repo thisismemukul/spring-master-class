35c33cbf9f3d335a05c5f23eb9691a5d276ad835
thisismemukul <w41k3r.12@gmail.com>
2023-03-16 01:44:25 +0530

    Step 23 - Mixing XML Context with Component Scan for Beans defined with Annotation

--- 01-spring-in-depth/src/main/java/com/springcourse/spring/basics/springin5steps/SpringIn5StepsXMLContextApplication.java
+++ 01-spring-in-depth/src/main/java/com/springcourse/spring/basics/springin5steps/SpringIn5StepsXMLContextApplication.java
@@ -2,6 +2,8 @@ package com.springcourse.spring.basics.springin5steps;
 
 import com.springcourse.spring.basics.springin5steps.basic.BinarySearchImp;
 import com.springcourse.spring.basics.springin5steps.xml.XmlPersonDAO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -14,12 +16,16 @@ public class SpringIn5StepsXMLContextApplication {
 	//What are beans?
 	//What are the dependencies of  a bean?
 //	Where to search for a beans? => no need
+private static Logger LOGGER =  LoggerFactory.getLogger(SpringIn5StepsScopeApplication.class);
+
 	public static void main(String[] args) {
 
 		//BinarySearchImp binarySearch= new BinarySearchImp(new QuickSortAlgorithm());
 		try (ClassPathXmlApplicationContext applicationContext =
 					 new ClassPathXmlApplicationContext("applicationContext.xml")) {
 //				SpringApplication.run(SpringIn5StepsBasicApplication.class, args);
+			LOGGER.info("Beans Loaded -> {}",
+					(Object) applicationContext.getBeanDefinitionNames());
 			XmlPersonDAO personDao = applicationContext.getBean(XmlPersonDAO.class);
 			System.out.println(personDao);
 			System.out.println(personDao.getXmlJdbcConnection());
