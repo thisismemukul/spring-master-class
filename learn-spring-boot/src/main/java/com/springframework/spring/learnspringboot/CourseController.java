3167294c9a044b3d327cddb4025db9ab81bb80d9
thisismemukul <w41k3r.12@gmail.com>
2023-03-16 17:16:11 +0530

    Step 09 - Get Production Ready with Spring Boot - 1 - Profiles

--- learn-spring-boot/src/main/java/com/springframework/spring/learnspringboot/CourseController.java
+++ learn-spring-boot/src/main/java/com/springframework/spring/learnspringboot/CourseController.java
@@ -12,10 +12,15 @@ public class CourseController {
 // /Course
 // Course: id, name, author
 @RequestMapping("/courses")
+
 public List<Course> retriveAllCourses(){
     return Arrays.asList(
             new Course(1,"learn AWS","springframework"),
-            new Course(2,"learn DevOPS","springframework")
+            new Course(2,"learn DevOPS","springframework"),
+            new Course(3,"learn Azure","springframework"),
+            new Course(4,"learn Azure","hjh"),
+            new Course(5,"learn Azure","hgg")
+
     );
 }
 }
