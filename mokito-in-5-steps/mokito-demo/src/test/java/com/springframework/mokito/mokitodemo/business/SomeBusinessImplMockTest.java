2c36639a397a79a8481afcb23e96defa21be02a0
thisismemukul <w41k3r.12@gmail.com>
2023-03-16 12:25:36 +0530

    Step 04 - Simplifying Tests with Mockito Annotations - Mock, InjectMocks

--- mokito-in-5-steps/mokito-demo/src/test/java/com/springframework/mokito/mokitodemo/business/SomeBusinessImplMockTest.java
+++ mokito-in-5-steps/mokito-demo/src/test/java/com/springframework/mokito/mokitodemo/business/SomeBusinessImplMockTest.java
@@ -5,24 +5,40 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class SomeBusinessImplMockTest {
+	@Mock
+	private DataService dataServiceMock;
+	
+	@InjectMocks
+	private SomeBusinessImpl businessImpl;
 
 	@Test
 	void findTheGreatestFromAllData_basicScenario() {
-		DataService dataServiceMock = mock(DataService.class);
+//		DataService dataServiceMock = mock(DataService.class);
 		when(dataServiceMock.retriveAllData()).thenReturn(new int[] {25,15,5});
-		SomeBusinessImpl businessImpl = new SomeBusinessImpl(dataServiceMock);
+//		SomeBusinessImpl businessImpl = new SomeBusinessImpl(dataServiceMock);
 		assertEquals(25, businessImpl.findTheGreatestFromAllData());
 	}
 	@Test
 	void findTheGreatestFromAllData_OneValue(){
-		DataService dataServiceMock = mock(DataService.class);
+//		DataService dataServiceMock = mock(DataService.class);
 //		dataServiceMock.retriveAllData() => new int[] {25,15,5};
 		when(dataServiceMock.retriveAllData()).thenReturn(new int[] {35});
 //		DataServiceStub dataServiceStub = new DataServiceStub();
-		SomeBusinessImpl businessImpl = new SomeBusinessImpl(dataServiceMock);
+//		SomeBusinessImpl businessImpl = new SomeBusinessImpl(dataServiceMock);
 		assertEquals(35, businessImpl.findTheGreatestFromAllData());
+	}
+
+	@Test
+	void findTheGreatestFromAllData_EmptyArray(){
+		when(dataServiceMock.retriveAllData()).thenReturn(new int[] {});
+		assertEquals(Integer.MIN_VALUE, businessImpl.findTheGreatestFromAllData());
 	}
 
 }
