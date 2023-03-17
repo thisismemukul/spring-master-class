cfd893e55a56c266bee5c91c891f9b815a297462
thisismemukul <w41k3r.12@gmail.com>
2023-03-17 17:55:41 +0530

    Step 04 - Populate data into Person Table

--- 04-spring-jdbc-to-jpa/database-demo/src/main/resources/data.sql
+++ 04-spring-jdbc-to-jpa/database-demo/src/main/resources/data.sql
@@ -3,6 +3,12 @@ create table person
    id integer not null,
    name varchar(255) not null,
    location varchar(255),
-   birth_date timestamp,
    primary key(id)
 );
+
+INSERT INTO PERSON (ID, NAME, LOCATION) 
+VALUES(10001,  'Ranga', 'Hyderabad');
+INSERT INTO PERSON (ID, NAME, LOCATION) 
+VALUES(10002,  'James', 'New York');
+INSERT INTO PERSON (ID, NAME, LOCATION) 
+VALUES(10003,  'Pieter', 'Amsterdam');
