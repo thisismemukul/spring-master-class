59dc12ce4ee8ea16a065285a1a02c6c57226aac8
thisismemukul <w41k3r.12@gmail.com>
2023-03-19 01:53:37 +0530

    Step 14 - Defining Person Entity

--- 04-spring-jdbc-to-jpa/database-demo/src/main/java/com/springframework/database/databasedemo/entity/Person.java
+++ 04-spring-jdbc-to-jpa/database-demo/src/main/java/com/springframework/database/databasedemo/entity/Person.java
@@ -1,6 +1,13 @@
 package com.springframework.database.databasedemo.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Person {
+	@Id
+	@GeneratedValue
 	private int id;
 	private String name;
 	private String location;
