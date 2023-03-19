77e2d529255d32a0d98f432de132c2d9b39d0a4e
thisismemukul <w41k3r.12@gmail.com>
2023-03-19 20:54:58 +0530

    Step 01 : Setting up Your First Java Web Application

--- 01-spring-in-depth/src/main/java/com/springcourse/spring/basics/springin5steps/basic/SortAlgorithm.java
+++
@@ -1,5 +0,0 @@
-package com.springcourse.spring.basics.springin5steps.basic;
-
-public interface SortAlgorithm {
-    public int[] sort(int[] numbers);
-}
--- 01-spring-in-depth/src/main/java/com/springcourse/spring/basics/springin5steps/cdi/SomeCdiDao.java
+++
@@ -1,9 +0,0 @@
-package com.springcourse.spring.basics.springin5steps.cdi;
-
-import org.springframework.stereotype.Component;
-
-import javax.inject.Named;
-
-@Named
-public class SomeCdiDao {
-}
